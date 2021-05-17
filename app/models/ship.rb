9d2bc7406b15da2f63e526c4f5992c144ec8a135
Luciana Sugeta <lulusugeta@gmail.com>
2021-05-17 12:37:50 -0400

    commit tree a02fbc295881ef8a1a8c56b7415523e6cfd66843

--- app/models/ship.rb
+++ app/models/ship.rb
@@ -1,2 +1,24 @@
 class Ship
+    attr_accessor :name, :type, :booty
+    @@ships =[]
+
+    def initialize(params)
+        @name = params[:name]
+        @type = params[:type]
+        @booty = params[:booty]
+        save
+    end
+
+    def save
+        @@ships << self
+    end
+
+    def self.all
+       @@ships
+    end
+
+    def self.clear
+        @@ships = []
+    end
+
 end
